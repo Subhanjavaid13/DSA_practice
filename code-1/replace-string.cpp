11de9be259af6490be0615d8cc3f99e57a741f74
Subhanjavaid13 <subhanjavaid5616@gmail.com>
2026-01-07 18:35:10 +0500

    replace strings

--- code-1/replace-string.cpp
+++ code-1/replace-string.cpp
@@ -4,10 +4,19 @@
 using namespace std;
 
 
+string replaceString(string s){
+        string temp = "";
+        for(int i = 0; i<s.length(); i++){
+                
+        }
+}
+
+
 int main(){
 
 
-        string s;
+        string s = "My Name is Subhan";
+        cout<<"Replace string is : "<<replaceString(s)<<endl;
 
         return 0;
 }
