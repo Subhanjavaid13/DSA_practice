f0f6c14529986022d7309124dafa766722578333
Subhanjavaid13 <subhanjavaid5616@gmail.com>
2026-01-07 18:37:47 +0500

    strings more questions

--- code-1/replace-string.cpp
+++ code-1/replace-string.cpp
@@ -7,8 +7,14 @@ using namespace std;
 string replaceString(string s){
         string temp = "";
         for(int i = 0; i<s.length(); i++){
-                
+                if(s[i] == ' '){
+                        temp.push_back('@');
+                        temp.push_back('4');
+                }else{
+                        temp.push_back(s[i]);
+                }
         }
+        return temp;
 }
 
 
