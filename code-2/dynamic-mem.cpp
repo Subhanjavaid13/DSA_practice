961034f7390999a46d38f6aeb1f97b4baf1a5c05
Subhanjavaid13 <subhanjavaid5616@gmail.com>
2026-01-12 14:49:45 +0500

    array dynamic memory allocation

--- code-2/dynamic-mem.cpp
+++ code-2/dynamic-mem.cpp
@@ -1,15 +1,28 @@
 #include<iostream>
 using namespace std;
 
+int printSum(int *arr , int n){
+        int sum = 0;
+        for(int i =0; i<n ; i++){
+                sum += arr[i];
+        }
+        return sum;
+}
+
 int main(){
         int n;
-        cout<<"Enter Numbers : ";
+        cout<<"Enter : ";
         cin>> n;
 
         int *arr = new int[n];
 
-        for(int i = 0 ; i<n; i++){
+        cout<<"Enter Numbers : ";
+        for(int i = 0 ; i<n; i++){        
                 cin>>arr[i];
         }
+
+        int ans = printSum(arr,n);
+
+        cout << "sum is : "<<ans;
 
 }
