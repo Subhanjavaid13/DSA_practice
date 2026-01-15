5d6e27de7991dfa5d08fe7e9108363437510cb0c
Subhanjavaid13 <subhanjavaid5616@gmail.com>
2026-01-15 16:12:14 +0500

    binary search through recursion

--- code-2/binary-search.cpp
+++ code-2/binary-search.cpp
@@ -13,13 +13,21 @@ bool binarySearch(int *arr, int s , int e, int key){
                 return 1;
         }
         if(arr[mid]<key){
-                binarySearch(arr,)
+                return binarySearch(arr,mid+1,e,key);
+        }else{
+        return binarySearch(arr,s,mid-1,key);
         }
 }
 int main(){
 
         int arr[10] = {0,2,5,6,10,12,35,45,67,81};
         int size = 10;
-       
+        bool ans = binarySearch(arr,0,size-1,0 );
+        if(ans){
+                cout<<"Element is present ";
+        }else{
+                cout<<"Element is not present ";
+
+        }
         return 0;
 }
